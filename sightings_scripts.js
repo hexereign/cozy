4c833e1e4251b0dd75b7342f432f0d6dea60b546
hexereign <76860113+hexereign@users.noreply.github.com>
2021-05-08 01:48:14 -0400

    Update sightings_scripts.js

--- sightings_scripts.js
+++ sightings_scripts.js
@@ -3,7 +3,9 @@ $(document).ready(function(){
 	// Lab 11 Alien Sighting Form Final_Mihalos
 
 	$("h2").css("background-color", "#b2ec5d");
-	
+	$("h2").width("550px");
+
+
 	// step 1 - datepicker
 		$("#datepicker").datepicker({ 			
 			maxDate: new Date(),
@@ -12,10 +14,10 @@ $(document).ready(function(){
 		});
 
 
-	// step 2 - alien type using buttons
+	// step 2 - set alien type using buttons
 		$( "#type_select" ).buttonset();
 
-	// step 3 wight and height using sliders to adjust value		
+	// step 3 weight and height using sliders to adjust value		
 		$( "#slide_weight" ).slider({
 			value:0,
 			min: 0,
@@ -39,21 +41,33 @@ $(document).ready(function(){
         $( "#height" ).val( $( "#slide_height" ).slider( "value" ));
         
 	// step 4 number of eyes arms and legs using spinner
+		$(function(){
+			
+			$('input[type="number"]').nicenumber();
+		});
 
+		$("#eyes").attr("min", "0");
+		$("#eyes").attr("max", "20");	
+		$("#arms").attr("min", "0");
+		$("#arms").attr("max", "20");
+		$("#legs").attr("min", "0");
+		$("#legs").attr("max", "20");
 
 	// step 5 color mixer for a swatch using slider
 	function refreshSwatch() {
 
-		var	red = $( "#red" ).slider( "value" );
-		var	green = $( "#green" ).slider( "value" );
-		var	blue = $( "#blue" ).slider( "value" );
+		var	red = $("#red").slider( "value" );
+		var	green = $("#green").slider( "value" );
+		var	blue = $("#blue").slider( "value" );
 		var	my_rgb = "rgb(" + red + "," + green + "," + blue + ")"; 
-			
-			$( "#swatch" ).css( "background-color", my_rgb );
-			$( "#red_val" ).val(red );
-			$( "#blue_val" ).val( blue);
-			$( "#green_val" ).val( green);
-			$( "#color_val" ).val( my_rgb);		
+			$("#swatch").width("100px");
+			$("#swatch").height("100px");
+			$("#swatch").css("background-image", "url('alien_svg.png')" );
+			$("#swatch").css( "background-color", my_rgb );
+			$("#red_val").val(red );
+			$("#blue_val").val( blue);
+			$("#green_val").val( green);
+			$("#color_val").val( my_rgb);		
 	}
 	$( "#red, #green, #blue" ).slider({
 		orientation: "horizontal",	
@@ -63,9 +77,9 @@ $(document).ready(function(){
 		slide: refreshSwatch,
 		change: refreshSwatch
 	});						
-	$( "#red" ).slider( "value", 127 );
-	$( "#green" ).slider( "value", 127 );
-	$( "#blue" ).slider( "value", 127 );
+	$("#red").slider("value", 127 );
+	$("#green").slider( "value", 127 );
+	$("#blue").slider( "value", 127 );
 
 
 	//5. Build a nicer-looking “submit“ button for the Sightings form.
@@ -83,7 +97,7 @@ $(document).ready(function(){
 		});
 		$("#sendReport").button();
 		$("#sendReport").click( ()=>
-			$("#dialog").dialog("open")
+		$("#dialog").dialog("open")
 		);
 
 
